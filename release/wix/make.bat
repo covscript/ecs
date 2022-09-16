d6f2d88e115b88618094df99b3148f742a0cf59b
李登淳 <mikecovlee@163.com>
2022-09-16 09:46:18 +0800

    Update make.bat

--- release/wix/make.bat
+++ release/wix/make.bat
@@ -1,4 +1,4 @@
 candle .\ecs_wix.wxs -nologo
-light -ext WixUIExtension -b . -cultures:en-us .\ecs_wix.wixobj -out extended_covscript_x86.msi -nologo
+light -ext WixUIExtension -b . -cultures:en-us .\ecs_wix.wixobj -out extended-covscript-x86.msi -nologo
 del /Q ecs_wix.wixobj
 del /Q extended_covscript_x86.wixpdb
