1e80aa7505d72f20bbf81359c396584ff175b6ef
李登淳 <mikecovlee@163.com>
2022-12-14 16:39:18 +0800

    make ecs can install using cspkg

--- bin/ecs.bat
+++ bin/ecs.bat
@@ -1,6 +1,6 @@
 @echo off
 set CURRENT_FOLDER=%~dp0
-set ARGS="%CURRENT_FOLDER%\..\ecs.csc"
+set ARGS="%CURRENT_FOLDER%\ecs.csc"
 :LOOP
     set index=%1
     if %index%! == ! goto END
@@ -8,4 +8,4 @@ set ARGS="%CURRENT_FOLDER%\..\ecs.csc"
     shift
     goto LOOP
 :END
-cs -i "%CURRENT_FOLDER%\..\imports" %ARGS%
+cs %ARGS%
