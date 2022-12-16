20b549b4078ba3253ba1e5a158911557f5769008
李登淳 <mikecovlee@163.com>
2022-12-16 20:47:00 +0800

    remove wix

--- release/wix/README.md
+++
@@ -1,4 +0,0 @@
-## Build instruction
-1. Copying `/bin/`, `/imports` and `ecs.csc` into this folder
-2. Copying `sdk_extension.cse` and `bitwise.cse` into `/imports` folder
-3. Run `make.bat`
--- release/wix/make.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd %~dp0
-candle .\ecs_wix.wxs -nologo
-light -ext WixUIExtension -b . -cultures:en-us .\ecs_wix.wixobj -out extended-covscript-x64.msi -nologo
-del /Q ecs_wix.wixobj
-del /Q extended-covscript-x64.wixpdb
